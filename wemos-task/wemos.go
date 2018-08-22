3000f8b0fac96ebf55f553e232a16c1ada94a904
Blake Yang <blake@wattx.io>
2018-08-22 11:10:30 +0200

    task 1 completed

--- wemos-task/wemos.go
+++ wemos-task/wemos.go
@@ -2,36 +2,42 @@ package main
 
 import (
 	"log"
-	_ "time"
+	"time"
 
 	"gobot.io/x/gobot"
-	_ "gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/firmata"
 )
 
 // Board pin mapping to ESP8266 GPIO pin numbers
-const  D0  = "16"
-const  D1  = "5"
-const  D2  = "4"
-const  D3  = "0"
-const  D4  = "2"
-const  D5  = "14"
-const  D6  = "12"
-const  D7  = "13"
-const  D8  = "15"
-const  D9  = "3"
-const  D10 = "1"
+const D0 = "16"
+const D1 = "5"
+const D2 = "4"
+const D3 = "0"
+const D4 = "2"
+const D5 = "14"
+const D6 = "12"
+const D7 = "13"
+const D8 = "15"
+const D9 = "3"
+const D10 = "1"
 const LED_BUILTIN = "2"
 
 // Wemos describes esp
 type Wemos struct {
 	firmata *firmata.TCPAdaptor
+	ledBI   *gpio.LedDriver
+	ledExt  *gpio.LedDriver
 }
 
 // NewWemos constructs new struct
 func NewWemos(f *firmata.TCPAdaptor) *Wemos {
+	ledBI := gpio.NewLedDriver(f, LED_BUILTIN)
+	ledExt := gpio.NewLedDriver(f, D5)
 	return &Wemos{
 		firmata: f,
+		ledBI:   ledBI,
+		ledExt:  ledExt,
 	}
 }
 
@@ -40,6 +46,7 @@ func (w *Wemos) Start() {
 	robot := gobot.NewRobot(
 		"bot",
 		[]gobot.Connection{w.firmata},
+		[]gobot.Device{w.ledBI, w.ledExt},
 		w.work,
 	)
 
@@ -48,4 +55,10 @@ func (w *Wemos) Start() {
 
 func (w *Wemos) work() {
 	log.Println("Robot starts working...")
+	gobot.Every(1*time.Second, w.toggleLed)
+}
+
+func (w *Wemos) toggleLed() {
+	w.ledBI.Toggle()
+	w.ledExt.Toggle()
 }
