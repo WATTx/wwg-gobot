76f1d3e50de33aa2e4a72ce1b80982bc0d400956
Blake Yang <blake@wattx.io>
2018-08-22 11:02:23 +0200

    task 1

---
+++ wemos-task/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/namsral/flag"
+
+	"gobot.io/x/gobot/platforms/firmata"
+)
+
+var (
+	firmataURL = flag.String("firmata_url", "", "firmata TCP address")
+)
+
+func main() {
+	flag.Parse()
+
+	// new adaptors
+	firmataAdaptor := firmata.NewTCPAdaptor(*firmataURL)
+
+	// new wemos
+	wemos := NewWemos(firmataAdaptor)
+	wemos.Start()
+}
